3721b6aed837dac9b97fff1b53258e9eba40a9b8
Carl-Vincent Landry-Duval <carl.vincent.ld@gmail.com>
2021-05-30 22:01:46 -0400

    Ajout de l'importation du pokedex (transfert)

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+pokedex_path = File.join(Rails.root, "db", "data", "pokedex.csv")
+PokedexEntry.transaction do
+  CSV.foreach(pokedex_path , skip_blanks: true, headers: true) do |row|
+    entry = PokedexEntry.new(
+      no: Integer(row["#"]),
+      name: row["Name"],
+      type1: row["Type 1"],
+      type2: row["Type 2"],
+      hp: Integer(row["HP"]),
+      attack: Integer(row["Attack"]),
+      defense: Integer(row["Defense"]),
+      spAtk: Integer(row["Sp. Atk"]),
+      spDef: Integer(row["Sp. Def"]),
+      speed: Integer(row["Speed"]),
+      generation: Integer(row["Generation"]),
+      legendary: 
+        case row["Legendary"].downcase
+          when "true" then true
+          when "false" then false
+          else nil
+        end
+    )
+    entry.save!
+  end
+end
